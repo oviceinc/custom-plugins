67211a43efa62e0a8d4c01ac875a92fc1642da3b
hanios123 <hanios123@gmail.com>
2023-11-13 19:46:03 +0100

    adjust container layout

--- react-app-v2/src/components/Container.tsx
+++ react-app-v2/src/components/Container.tsx
@@ -1,8 +1,22 @@
-import Paper from "@mui/material/Paper";
+import { Stack } from "@mui/material";
 import { ReactElement } from "react";
 type ContainerProps = {
   children: ReactElement;
 };
 export const Container = ({ children }: ContainerProps) => {
-  return <Paper elevation={3}>{children}</Paper>;
+  return (
+    <Stack
+      justifyContent={"center"}
+      sx={{
+        position: "absolute",
+        backgroundColor: "white",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+      }}
+    >
+      {children}
+    </Stack>
+  );
 };
